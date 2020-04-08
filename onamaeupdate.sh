bcf25b83484819c1d72b4fb6af91ecf4edf2eba4
sina <sina.moemoe@gmail.com>
2020-04-08 13:06:37 +0900

    password exported to ./user.txt  &  add date

--- onamaeupdate.sh
+++ onamaeupdate.sh
@@ -7,20 +7,25 @@ DOMNAME="【更新するドメイン名】"
 UNAME="【お名前.comのユーザ名】"
 PASSWD="【お名前.comのパスワード】"
 
+. ./user.txt
+
 #
 # 1.get external IP for IP change
 #
 
+echo -n `date`
 IP1=` curl -s https://ugtop.com/ | grep IP |tr "<" " "  | awk '{ print $3 }' `
-#echo $IP1
-
+echo -n "    $IP1"
 IP2=`dig @01.dnsv.jp $HOSTNAME.$DOMNAME +short`
-#echo $IP2
+echo -n "    $IP2"
 
 #
 # 2. if IP is changed , then update to onamae.com
 #
 if [ $IP1 != $IP2  ]
 then
+  echo "    NG "
   expect ./onamae.exp $IP1 $HOSTNAME $DOMNAME $UNAME $PASSWD
+else
+  echo "    OK "
 fi
